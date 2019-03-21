d642bc6c6ba41fa65e7474d284d10f24e3a66848
albCh <alberto.chang.lopez@gmail.com>
2019-03-21 10:38:59 -0500

    new, edit , show labs

--- app/controllers/laboratories_controller.rb
+++ app/controllers/laboratories_controller.rb
@@ -7,17 +7,43 @@ class LaboratoriesController < ApplicationController
   end
 
   def new
+    @laboratory = Laboratory.new
   end
 
   def create
+    @laboratory = Laboratory.new laboratory_params
+    if @laboratory.save
+      #Audit.register current_user, @action, @controller, register_status = true
+      redirect_to laboratories_path
+    else
+      #Audit.register current_user, @action, @controller, register_status = false
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @laboratory.update_attributes laboratory_params
+      #Audit.register current_user, @action, @controller, register_status = true
+      redirect_to laboratories_path
+    else
+      #Audit.register current_user, @action, @controller, register_status = false
+      render :edit
+    end
   end
 
   def show
+  end
+
+  private
+
+  def laboratory_params
+    params.require(:laboratory).permit(:id, :name, :email, :address, :phone, :description)
+  end
+
+  def set_laboratory
+    @laboratory = Laboratory.find params[:id]
   end
 end
